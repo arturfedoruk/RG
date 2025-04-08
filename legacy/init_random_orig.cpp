33b75bec31b39911a7919486b0dce3cbeca143da
arturfedoruk <116838078+arturfedoruk@users.noreply.github.com>
2025-04-08 22:04:57 +0300

    beautification init_random_orig.cpp

--- legacy/init_random_orig.cpp
+++ legacy/init_random_orig.cpp
@@ -1,5 +1,6 @@
+// basically init_random but with loop_Fit_Inputs_original()
 // to launch the program: 
-// g++ `root-config --cflags` withSMDRnROOTinitrandomOrig.cpp `root-config --libs` -lm -lsmdr -ltsil -l3vil
+// g++ `root-config --cflags` init_random_orig.cpp `root-config --libs` -lm -lsmdr -ltsil -l3vil
 
 #include "smdr.h"
 #include "iostream"
@@ -10,9 +11,9 @@
 #include "TTree.h"
 #include "TFile.h"
 using namespace std;
-#define ZEROSAFE(a) (((a) > (SMDR_TOL)) ? (a) : (SMDR_TOL)) //idk wht's that
 
-#include "my_Fit_Inputs_original.cpp"
+#include "../loop_configs.cpp"
+#include "../loop_Fit_Inputs_original.cpp"
 
 int main(){
 
@@ -30,15 +31,6 @@ int main(){
 	TTree* trees[nconfigs] = {tree_111111, tree_222222, tree_333333, tree_333221, tree_444332, tree_444333};
 	
 	char* tree_names[nconfigs] = {"tree_111111", "tree_222222", "tree_333333", "tree_333221", "tree_444332", "tree_444333"};
-	
-	float config_111111[9] = {0, 0, 0, 0, 0, 0, 1, 1} ; // for QCDQED_at_MZ & mbmb loop 0 doesn't exist
-	float config_222222[9] = {1, 1, 1, 1, 1, 1, 1, 1} ;
-	float config_333333[9] = {2, 2, 2, 2, 2, 2, 2, 2} ;
-	float config_333221[9] = {2, 2, 0, 2, 2, 0, 2, 1} ;
-	float config_444332[9] = {3, 2, 1, 2.5, 2.5, 1, 3, 2} ; 
-	float config_444333[9] = {3, 2, 2, 2.5, 2.5, 2, 3, 2} ; // hfor MZ & MW loop 3 doesn't exist
-	
-	float* loop_configs[nconfigs] = {config_111111, config_222222, config_333333, config_333221, config_444332, config_444333};
 	
 	int seed;
 	double alphaS_MZ, alpha, GFermi, MZ, Mh, Mt, mbmb, Delta_alpha, g, gp, g3, yt, yb, lambda, x1, x2, x3, y1, y2, z;
@@ -83,7 +75,7 @@ int main(){
 		mbmb = SMDR_mbmb_EXPT;
 		Delta_alpha = SMDR_Delta_alpha_had_5_MZ_EXPT;
 		
-		my_Fit_Inputs_original (SMDR_Q_in,
+		loop_Fit_Inputs_original (SMDR_Q_in,
 		           SMDR_alphaS_MZ_EXPT,
 		           SMDR_alpha_EXPT,
 		           SMDR_GFermi_EXPT,
@@ -115,10 +107,6 @@ int main(){
 	}
 	
 	file->Close();
-	
-	ofstream lastSeed("lastSeed.txt");
-	lastSeed << 0 << endl;
-	lastSeed.close();
 	
 	return 0;
 
